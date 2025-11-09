056484a871c1fe5efefaaedef6a5fb5c77eed7b8
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-11-09 08:31:22 +0700

    modifbilangan2

--- arrayBilanganmodif.java
+++ arrayBilanganmodif.java
@@ -2,15 +2,12 @@ import java.util.Scanner;
 public class arrayBilanganmodif {
 
     public static void main(String[] args) {
-        int [] bil = {5,13,-7,17};
-
-    
-        System.out.println(bil [0]);
-        System.out.println(bil [1]);
-        System.out.println(bil [2]);
-        System.out.println(bil [3]);
        
-          
+       int bil [] = {5,13,-7,7}; 
+        
+        for (int i =0; i < 4; i++ ) {
+        System.out.println(bil [i]);
+       }  
     } 
 }
 
