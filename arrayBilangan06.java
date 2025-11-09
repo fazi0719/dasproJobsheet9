f3edd27ae7f89e4539b03199391e393950f5326e
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-11-09 08:41:53 +0700

    percobaan 1

--- arrayBilangan06.java
+++ arrayBilangan06.java
@@ -1,12 +1,9 @@
 import java.util.Scanner;
 public class  arrayBilangan06 {
     public static void main(String[] args) {
-        int [] bil = new int [4];
+        int [] bil = {5,13,-7,17};
 
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
+        
 
         System.out.println(bil [0]);
         System.out.println(bil [1]);
