6a40ce932cffe5c83aff7cb6ecf212213e5d5a3b
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-11-10 15:58:22 +0700

    modif no5

---
+++ arrayNilaimodif06.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class arrayNilaimodif06 {
+    
+public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int [] nilaiAkhir = new int [10];
+
+        for (int i = 0; i < nilaiAkhir.length; i++){
+            System.out.print("Masukkan nilai akhir ke- "+i+" : ");
+            nilaiAkhir [i] = sc.nextInt();
+        }
+
+            for (int o =0; o < 10; o++){
+                System.out.println("Nilai akhri ke-"+o+" adalah "+ nilaiAkhir[o]);
+            }
+        }
+    }
+
+
