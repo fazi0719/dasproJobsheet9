8bd788f3d17fea612d5f7489d7e2c0509b50edff
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-11-11 00:29:23 +0700

    pertanyaan1.3

--- arrayBilanganmodif.java
+++ arrayBilanganmodif.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class arrayBilanganmodif {
 
     public static void main(String[] args) {
-       
        int bil [] = {5,13,-7,7}; 
         
         for (int i =0; i < 4; i++ ) {
