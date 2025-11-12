d2e308f77871e775ab267585e5b99a356aad4124
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-11-12 12:10:29 +0700

    modif no 2

--- SearchNilai06.java
+++ SearchNilai06.java
@@ -2,19 +2,33 @@ import java.util.Scanner;
 public class SearchNilai06{
     
 public static void main(String[] args) {
-    final int [] arrNilai = { 80, 85, 78, 96, 90,82, 86} ; 
-    int key = 90;
-    int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+ 
+int jumlah =0, key = 0, hasil =0;
 
-for ( int i = 0; i < arrNilai.length; i++){
-    if ( key == arrNilai[i]){
-        hasil=i;
-        break;
+System.out.print("Masukkan nilai yang akan diinput :");
+ jumlah = sc.nextInt();
+    int [] arrNilai = new int [jumlah];
+     
+
+    for ( int i = 0; i < arrNilai.length;i++){
+        System.out.print("Masukkan nilai mahasiswa ke-" + (i+1)+ ":");
+            arrNilai[i]=sc.nextInt();
     }
+
+System.out.print("Masukkan nilai yang akan dicari:");
+ key = sc.nextInt ();
+
+for ( int i = 0; i < arrNilai.length;i++){
+if ( key == arrNilai[i]){
+hasil=i;
+break;
+}
 }
 
 System.out.println();
-System.out.println(" Nilai " +key+" ketemu di indeks ke-"+hasil);
-System.out.println();
+if (hasil != 0){
+System.out.println(" Nilai " +key+ " ketemu, merupakan nilai mahasiswa ke-" + (hasil+1));
+}
 }
 }
